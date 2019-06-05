d176b12cb4a8263c8f751cccc9e5b0c82cc1a2ee
unknown <cmzolczynski@gmail.com>
2019-06-04 22:19:55 -0700

    finished adding questions answers and correct answers variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,11 @@ var questions= [
     "Who said: I don't know half of you half as well as I should like, and I like less than half of you half as well as you deserve.",
     "Who is Shelob",
     "The movie does not mention it, but Bilbo shares his birthday with which hobbit?",
+    "Hobbits eat seven meals a day. What comes directly after elevensies?",
+    "Denethor is the 26th what?",
+    "Who became King of Rohan after Theoden met his demise?",
+    "How many wizards are there in Middle Earth?",
+    "What is the name of the volcano that the Ring is thrown into?"
 
 ];
 
@@ -13,7 +18,11 @@ var answers= [
     ["Bilbo Baggins", "Frodo", "Gandalf", "Pippin"],
     ["An ent that befriends Pippin and Merry", "A beastly spider that tries to eat Frodo and Sam", "An elf queen", "Uruk-hai leader"],
     ["Pippin", "Smeagol", "Frodo", "Belladonna Took Baggins"],
-
+    ["Afternoon tea", "Dinner", "Supper", "Luncheon"],
+    ["King of Gondor", "Stewart of Gondor", "King of Rohan", "Stewart of Arnor"],
+    ["Eowyn", "Grima", "Eorl", "Eomer"],
+    ["3", "5", "9", "17"],
+    ["Barad Dur", "Osgiliath", "Cirith Ungol", "Minas Tirith"]
 ];
 
 var correctAnswers= [
@@ -22,6 +31,11 @@ var correctAnswers= [
     "Bilbo Baggins",
     "A beastly spider that tries to eat Frodo and Sam",
     "Frodo",
-]
+    "Luncheon",
+    "Stewart of Gondon",
+    "Eomer",
+    "5",
+    "Barad Dur"
+];
 
 
