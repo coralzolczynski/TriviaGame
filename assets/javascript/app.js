dbe0dd1ad811266df2d9b694222a89d129863fb5
unknown <cmzolczynski@gmail.com>
2019-06-04 21:54:49 -0700

    added question and answer var

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+var questions= [
+    "How many rings of power were forged in the second age?",
+    "In Fellowship of the Ring, what gift does Lady Galadriel give Gimli before the fellowship leave Lothlorien?",
+    "Who said: I don't know half of you half as well as I should like, and I like less than half of you half as well as you deserve.",
+    "Who is Shelob",
+    "The movie does not mention it, but Bilbo shares his birthday with which hobbit?",
+
+];
+
+var answers= [
+    ["1", "13", "19", "20"],
+    ["Elvish rope", "Three strands of her hair", "a drinking horn crafted from wood", "a dagger"],
+    ["Bilbo Baggins", "Frodo", "Gandalf", "Pippin"],
+    ["An ent that befriends Pippin and Merry", "A beastly spider that tries to eat Frodo and Sam", "An elf queen", "Uruk-hai leader"],
+    ["Pippin", "Smeagol", "Frodo", "Belladonna Took Baggins"],
+
+];
+
+var correctAnswers= [
+    "20", 
+    "Three strands of her hair",
+    "Bilbo Baggins",
+    "A beastly spider that tries to eat Frodo and Sam",
+    "Frodo",
+]
+
+
