e055f93d00e1ef9aa1fb26780f80c7026178abd2
unknown <cmzolczynski@gmail.com>
2019-06-06 17:16:16 -0700

    adding functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+$(document).ready(function () {
+    //create Game Start button
+    function createStart() {
+        var startPage = $("<button class='startButton btn text-center' type='button'>Game Start!</button>");
+        $(".body").html(startPage);
+    };
+
+
+    createStart();
+    game();
+    
+
 var questions= [
     "How many rings of power were forged in the second age?",
     "In Fellowship of the Ring, what gift does Lady Galadriel give Gimli before the fellowship leave Lothlorien?",
@@ -26,7 +38,7 @@ var answers= [
     ["Sting", "Anduril", "Narsil", "Glamdring"]
 ];
 
-var correctAnswers= [
+var correctAns= [
     "20", 
     "Three strands of her hair",
     "Bilbo Baggins",
@@ -39,6 +51,15 @@ var correctAnswers= [
     "Barad Dur",
     "Anduril"
 ];
+
+var questionCount = 0;
+var counter = 30;
+var time;
+var correct = 0;
+var wrong = 0;
+var unanswer = 0;
+var timeout;
+var box;
 
 function countdown(){
     if (counter > 0) {
@@ -55,3 +76,82 @@ function countdown(){
 function timeOut() {
     time = setInterval(countdown, 1000);
 };
+
+function createTrivia() {
+    var main = $("<div class='main'></div>");
+    var timer = $("<div class='remain'>Time remaining is : <span class='timer'>30</span></div>");
+    box = $("<div class='box'></div>");
+
+    var question = $("<div class='question'>question</div>");
+    var answersChoice = $("<div class='answerChoice'>Choose your answer from below:</div>");
+    var firstAnswer = $("<div class='ans firstAnswer'>1stanswer</div>");
+    var secondAnswer = $("<div class='ans secondAnswer'>2ndanswer</div>");
+    var thirdAnswer = $("<div class='ans thirdAnswer'>3ndanswer</div>");
+    var fourthAnswer = $("<div class='ans fourthAnswer'>4thanswer</div>");
+    box = $("<div class='box'></div>");
+
+    main.append(timer).append(box);
+    box.html(question).append(answersChoice);
+    answersChoice.append(firstAnswer).append(secondAnswer).append(thirdAnswer).append(fourthAnswer);
+    $(".body").html(main);
+};
+
+function start() {
+    createTrivia();
+    timeOut();
+    assignData();
+    pickAns();
+  };
+
+  function game() {
+      $(".startButton").on("click", function (){
+          start();
+      });
+  };
+
+function assignData() {
+    $(".question").text(question[questionCount]);
+    $(".firstAnswer").text(answers[questioncount][0]);
+    $(".secondAnswer").text(answers[questionCount][1]);
+    $(".thirdAnswer").text(answers[questionCount][2]);
+    $(".fourthAnswer").text(answers[questionCount][3]);
+
+};
+
+function pickAns() {
+    if (this.innerHTML === correctAns[questionCount]) {
+        ifCorrect();
+    } else {
+        ifIncorrect();
+    }
+};
+
+function ifCorrect() {
+    correct++;
+    var correctAnswer = $("<div class='correctAnswer'>Correct! The answer is: <span>" + correctAns[questionCount] + " </span></div>");
+    $(".box").html(correctAnswer);
+    clearInterval(time);
+    next();
+    end();
+};
+
+function ifIncorrect() {
+    wrong++;
+    var correctAnswer = $("<div class='correctAnswer'>Wrong! The answer is: <span>" + correctAns[questionCount] + " </span></div>");
+    $(".box").html(correctAnswer);
+    clearInterval(time);
+    next();
+    end();
+};
+
+function ifOutOfTime() {
+    unanswer++;
+    var correctAnswer = $("<div class='correctAnswer'>Wrong! The answer is: <span>" + correctAns[questionCount] + " </span></div>");
+    $(".box").html(correctAnswer);
+    clearInterval();
+    next();
+    end();
+};
+
+
+
